d725b70c27c088ff3cca60b4db4e543b8ff08ab4
Terres <pedrocoutoterres@gmail.com>
2025-09-03 14:37:52 -0300

    class Pet with get and set

--- Pet.java
+++ Pet.java
@@ -1,3 +1,25 @@
 public class Pet {
-    
+    private String nome;
+    private String tipo;
+
+    public Pet(String nome, String tipo){
+        this.nome = nome;
+        this.tipo = tipo;
+    } 
+
+    //getters
+    public String getNome(){
+        return nome;
+    }
+    public String getTipo(){
+        return tipo;
+    }
+
+    //setters
+    public void setNome(String nome){
+        this.nome = nome;
+    } 
+    public void setTipo(String tipo){
+        this.tipo = tipo;
+    }
 }
